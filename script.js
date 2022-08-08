6fd496f6afaf56591f27fdb7e3b255687aa7ba64
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-08 19:27:46 -0300

    Resolvido o requisito 3 e 4 do projeto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function gerar_cor() {
+function genColors() {
   const r = parseInt(Math.random() * 255);
 
   const g = parseInt(Math.random() * 255);
@@ -6,12 +6,28 @@ function gerar_cor() {
   const b = parseInt(Math.random() * 255);
 
   const rgb = `(${r}, ${g}, ${b})`;
-  console.log(rgb);
+  return rgb;
+}
+
+function dispColor() {
+  rgb = genColors();
 
   const local = document.getElementById('local');
   const color = document.createElement('p');
   color.id = 'rgb-color';
   color.innerText = rgb;
   local.appendChild(color);
+  return rgb;
 }
-gerar_cor();
+dispColor();
+
+function genBalls() {
+  const local = document.getElementById('color-balls');
+  for (let index = 0; index < 6; index += 1) {
+    const ball = document.createElement('div');
+    ball.classList.add('ball');
+    ball.style.backgroundColor = `rgb${genColors()}`;
+    local.appendChild(ball);
+  }
+}
+genBalls();
